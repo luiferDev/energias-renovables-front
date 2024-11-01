7e5298e283058266b10447ac39c2f52011d532fc
Jorge Morales <luifer991@gmail.com>
2024-10-31 21:32:45 -0500

    first commit

---
+++ src/EJemplo2.jsx
@@ -0,0 +1,11 @@
+
+// eslint-disable-next-line react/prop-types
+export default function EJemplo2({ hola }) {
+    return (
+        <>
+            <h1 className="hola">{hola}</h1>
+            <h2>{hola}</h2>
+        </>
+        
+    )
+}
