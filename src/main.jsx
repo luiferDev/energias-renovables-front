387bc1bd1857f921f16f776a75bb2a286973eaa6
Jorge Morales <luifer991@gmail.com>
2024-11-03 18:26:14 -0500

    incluir componente theme en el punto de entrada

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Theme } from "@radix-ui/themes"
+import "@radix-ui/themes/styles.css"
 import './index.css'
 import App from './App.jsx'
-import EJemplo2 from './EJemplo2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <App />
-    <App />
-    <EJemplo2 hola={"Hola"} />
+    <Theme accentColor="crimson"
+      grayColor="gray" radius="full" scaling="100%"
+      panelBackground="solid"
+      appearance="dark" >
+      <App />
+    </Theme>
   </StrictMode>
 )
