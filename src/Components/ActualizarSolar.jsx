9e207c5ccf0958fa93d5a23e124d0bd06e7e563e
Jorge Morales <luifer991@gmail.com>
2024-11-05 23:38:31 -0500

    instalar react hook form y hacer el formulario manejandolo por consola TODO: enviar el formulario a la API para actualizar los datos por medio del ID

--- src/Components/ActualizarSolar.jsx
+++ src/Components/ActualizarSolar.jsx
@@ -2,47 +2,55 @@
 import { Container } from '@radix-ui/themes'
 import '../../src/styles/actualizar-solar.css'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form';
 
 export default function ActualizarSolar() {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(JSON.stringify(data));
+
     return (
         <Container size="1" align={'center'} pt={"9"}>
-            <form className="form">
+            <form onSubmit={handleSubmit(onSubmit)} className="form">
                 <p id="heading">Actualizar Energía Solar</p>
                 <div className="field">
-                    <input placeholder="Nombre País" className="input-field" type="text" />
+                    <input {...register("nombre")} placeholder="Nombre País" className="input-field" type="text" />
                 </div>
                 <div className="field">
-                    <input placeholder="Energía Requerida" className="input-field" type="number" />
+                    <input {...register("energiaRequerida")} placeholder="Energía Requerida" className="input-field" type="number" />
                 </div>
                 <div className="field">
-                    <input placeholder="Nivel Covertura" className="input-field" type="number" />
+                    <input {...register("nivelCovertura")} placeholder="Nivel Covertura" className="input-field" type="number" />
                 </div>
                 <div className="field">
-                    <input placeholder="Población" className="input-field" type="number" />
+                    <input {...register("poblacion")} placeholder="Población" className="input-field" type="number" />
                 </div>
                 <div className="field">
-                    <input placeholder="Ubicación" className="input-field" type="number" />
+                    <input {...register("ubicacion")} placeholder="Ubicación" className="input-field" type="text" />
                 </div>
                 <div className="field">
-                    <input placeholder="Capacidad Instalada" className="input-field" type="number" />
+                    <input {...register("capacidad_instalada")} placeholder="Capacidad Instalada" className="input-field" type="number" />
                 </div>
                 <div className="field">
-                    <input placeholder="Eficiencia" className="input-field" type="number" />
+                    <input {...register("eficiencia")} placeholder="Eficiencia" className="input-field" type="number" />
                 </div>
                 <div className="field">
-                    <input placeholder="Fecha Creación" className="input-field" type="date" />
+                    <input {...register("fecha_creacion")} placeholder="Fecha Creación" className="input-field" type="date" />
                 </div>
                 <div className="field">
-                    <input placeholder="Código" className="input-field" type="number" />
+                    <input {...register("nombre")} placeholder="Código" className="input-field" type="text" />
                 </div>
                 <div className="field">
-                    <input placeholder="Radiación Solar Promedio" className="input-field" type="number" />
+                    <input {...register("radiacionSolarPromedio")} placeholder="Radiación Solar Promedio" className="input-field" type="number" />
                 </div>
                 <div className="field">
-                    <input placeholder="Area Paneles" className="input-field" type="number" />
+                    <input {...register("areaPaneles")} placeholder="Area Paneles" className="input-field" type="number" />
                 </div>
                 <div className="field">
-                    <input placeholder="Angulo Inclinación" className="input-field" type="number" />
+                    <input {...register("anguloInclinacion")} placeholder="Angulo Inclinación" className="input-field" type="number" />
+                </div>
+                <div className="field">
+                    <input {...register("eficienciaPaneles")} placeholder="Eficiencia Paneles" className="input-field" type="number" />
                 </div>
                 <div className="btn">
                     <button className="button1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Guardar&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
