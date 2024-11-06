2f69c556454c3f17d0c23b6deaedd4f540e0a23a
Jorge Morales <luifer991@gmail.com>
2024-11-05 23:20:24 -0500

    actualizar formulario

--- src/Components/ActualizarSolar.jsx
+++ src/Components/ActualizarSolar.jsx
@@ -1,6 +1,7 @@
 
 import { Container } from '@radix-ui/themes'
 import '../../src/styles/actualizar-solar.css'
+import { Link } from 'react-router-dom'
 
 export default function ActualizarSolar() {
     return (
@@ -13,11 +14,44 @@ export default function ActualizarSolar() {
                 <div className="field">
                     <input placeholder="Energía Requerida" className="input-field" type="number" />
                 </div>
-                <div className="btn">
-                    <button className="button1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Login&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
-                    <button className="button2">Sign Up</button>
+                <div className="field">
+                    <input placeholder="Nivel Covertura" className="input-field" type="number" />
                 </div>
-                <button className="button3">Forgot Password</button>
+                <div className="field">
+                    <input placeholder="Población" className="input-field" type="number" />
+                </div>
+                <div className="field">
+                    <input placeholder="Ubicación" className="input-field" type="number" />
+                </div>
+                <div className="field">
+                    <input placeholder="Capacidad Instalada" className="input-field" type="number" />
+                </div>
+                <div className="field">
+                    <input placeholder="Eficiencia" className="input-field" type="number" />
+                </div>
+                <div className="field">
+                    <input placeholder="Fecha Creación" className="input-field" type="date" />
+                </div>
+                <div className="field">
+                    <input placeholder="Código" className="input-field" type="number" />
+                </div>
+                <div className="field">
+                    <input placeholder="Radiación Solar Promedio" className="input-field" type="number" />
+                </div>
+                <div className="field">
+                    <input placeholder="Area Paneles" className="input-field" type="number" />
+                </div>
+                <div className="field">
+                    <input placeholder="Angulo Inclinación" className="input-field" type="number" />
+                </div>
+                <div className="btn">
+                    <button className="button1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Guardar&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+                    <Link to={'/'}>
+                        <button className="button2">Cancelar</button>
+                    </Link>
+
+                </div>
+                {/* <button className="button3">Forgot Password</button> */}
             </form>
         </Container>
     )
